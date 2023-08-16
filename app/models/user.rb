9c404fe168cb4b7f741f137fb91eb9498c8ec76f
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-16 10:06:16 +0900

    ユーザー管理機能テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,22 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   with_options presence: true do 
-    validates :nickname, uniqueness: true
-    validates :email, uniqueness: true
-    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
     validates :date_of_birth
-  end       
+  end    
+  with_options allow_blank: true do
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+      validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+  end   
 end
