0ff4bb868e093a1d489c02a9392f2c0d0ea681f1
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-18 07:14:26 +0900

    正常系のテストが準備

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,39 +4,46 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do
-    it 'メールアドレスが必須であること' do
-      @user.email = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-    it 'メールアドレスが一意性であること' do
-      @user.save
-      @user2 = FactoryBot.build(:user)
-      @user2.email = @user.email
-      @user2.valid?
-      expect(@user2.errors.full_messages).to include('Email has already been taken')
-    end
-    it 'メールアドレスは、@を含む必要があること' do
-      @user.email = 'invalidemail'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email is invalid')
-    end
-    it 'パスワードが必須であること' do
-      @user.password = ''
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
-    end
-    it 'パスワードは、6文字以上での入力が必須であること' do
-      @user.password = '12345'
-      @user.password_confirmation = '12345'
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
-    end
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmationとlast_name,first_name,last_name_kana,first_name_kana,last_name_kana,date_of_birthが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      end
+    context '新規登録できないとき' do
+      it 'メールアドレスが必須であること' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'メールアドレスが一意性であること' do
+        @user.save
+        @user2 = FactoryBot.build(:user)
+        @user2.email = @user.email
+        @user2.valid?
+        expect(@user2.errors.full_messages).to include('Email has already been taken')
+      end
+      it 'メールアドレスは、@を含む必要があること' do
+        @user.email = 'invalidemail'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'パスワードが必須であること' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'パスワードは、6文字以上での入力が必須であること' do
+        @user.password = '12345'
+        @user.password_confirmation = '12345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
 
-    it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
-      @user.password_confirmation = 'mismatched_password'
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      it 'パスワードとパスワード（確認）は、値の一致が必須であること' do
+        @user.password_confirmation = 'mismatched_password'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
     end
   end
 end
