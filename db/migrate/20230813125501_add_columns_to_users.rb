57b07a85b88f8ea6d9d204ab0a5c5c8cfe70088c
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-18 06:57:05 +0900

    migrate修正

--- db/migrate/20230813125501_add_columns_to_users.rb
+++ db/migrate/20230813125501_add_columns_to_users.rb
@@ -1,10 +1,10 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :nickname, :string
-    add_column :users, :last_name, :string
-    add_column :users, :first_name, :string
-    add_column :users, :last_name_kana, :string
-    add_column :users, :first_name_kana, :string
-    add_column :users, :date_of_birth, :date
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :last_name, :string, null: false
+    add_column :users, :first_name, :string, null: false
+    add_column :users, :last_name_kana, :string, null: false
+    add_column :users, :first_name_kana, :string, null: false
+    add_column :users, :date_of_birth, :date, null: false
   end
 end
