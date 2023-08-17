efd76de80526511164b341219d2f74d832fa43cc
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-18 06:59:07 +0900

    passwordに英数字が含まれるように修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     date_of_birth         { Faker::Date.backward }
     first_name            { Faker::Japanese::Name.first_name }
