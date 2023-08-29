0bf73c55708e3d3790617c1a407cae8c4a9af3f6
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-30 07:45:42 +0900

    テストエーラ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,23 +7,27 @@ class Item < ApplicationRecord
     belongs_to :product_status
     belongs_to :shipping_fee
     belongs_to :prefecture
-    belongs_to :shipping_duration
+    belongs_to :shipping_duriation
 
     has_one_attached :image
 
-    with_options presence: true, numericality: { greater_than: 0 }, exclusion: { in: ["---"] } do
+    with_options presence: true do
+        
+        validates :image
+        validates :name
+        validates :description
+        validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+        validates :user_id 
+
+    end
+
+    with_options presence: true, numericality: { greater_than: 0 },exclusion: { in: ["---"] } do
         validates :category_id
         validates :product_status_id
         validates :shipping_fee_id
         validates :prefecture_id
-        validates :shipping_duration_id
-    end
-
-        validates :image, presence: true
-        validates :name, presence: true
-        validates :description, presence: true
-        validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-
-
+        validates :shipping_duriation_id
+      end
+    
     
 end
