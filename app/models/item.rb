6e8a8288d59d170f9fb7f0933569c8cd10005a92
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-30 15:10:38 +0900

    不要のを消す

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,6 @@ class Item < ApplicationRecord
         validates :name
         validates :description
         validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-        validates :user_id 
 
     end
 
