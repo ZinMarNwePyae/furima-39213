c294bbb19cc2618cb3a567dd60c20b8c9afce3a7
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-08-31 19:56:50 +0900

    実装前の段階を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show]
+  before_action :authenticate_user!, except: [:index]
+  #before_action :set_item, only: [:show]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -18,8 +18,8 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
+  #def show
+  #end
 
   private
 
