00c4543e26381bec0e7f7dd3dd0eff6ed2a9cc54
ZinMarNwePyae <132534409+ZinMarNwePyae@users.noreply.github.com>
2023-09-08 21:14:04 +0900

    テスト修正

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
     @order_form = FactoryBot.build(:order_form)
   end
   describe '配送先情報の保存' do
